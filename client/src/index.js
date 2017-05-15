eee4c5c154f2312ac9875e4afecf49e581f89490
xrr2016 <xiaoranran1993@163.com>
2017-05-15 22:22:43 +0800

    :zap: Application and Redux Setup

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './rootReducer'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux'
+import App from './App';
+import './index.css';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ client/src/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import games from './reducers/games'
+
+export default combineReducers({
+  games
+})
